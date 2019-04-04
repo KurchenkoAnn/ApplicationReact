b5f18a299e1973bc7d6995bfbcc234ded2f1e34d
Anet <Anet@A33COMP14.step.itstep.rv.ua>
2019-04-04 17:48:46 +0300

    Weather and TodoLost

--- src/TodoListApp.js
+++ src/TodoListApp.js
@@ -1,13 +1,156 @@
 import React, { Component } from 'react';
 import './App.css';
 
-export default class TodoList extends Component{
-render(){
+
+
+let todoitems = [];
+todoitems.push({index:1,value: "To do home task from Math",done : false} );
+todoitems.push({index:2, value: "To do home task from programmer",done:true  });
+todoitems.push({index:3, value: "Become older",done:false  });
+class TodoList extends Component{
+  render(){
+    var todoitems = this.props.items.map((item,index)=>{
+     return (<TodoListItem key={index} item={item} index={index} removeitem={this.props.removeitem}
+     markDone={this.props.markDone}
+     ></TodoListItem>);
+
+    });
+
+    return( <div className="list-group">{todoitems} </div>)
+  }
+
+}
+class TodoListItem extends Component{
+  constructor(props){
+    super(props);
+    this.onClickClose= this.onClickClose.bind(this);
+    this.onCliclDone= this.onCliclDone.bind(this);
+  }
+  onClickClose()
+  {
+var index= parseInt(this.props.index);
+this.props.removeitem(index);
+
+
+  }
+  onCliclDone()
+  {
+    let index = parseInt(this.props.index);
+    this.props.markDone(index);
+  }
+
+
+  render(){
+let todoClass= this.props.item.done ? "done" : "undone";
+return(
+    
+<div class="columns is-variable is-1-mobile is-0-tablet is-3-desktop is-8-widescreen is-2-fullhd">
+<div class="column">
+<div className={todoClass}>
+   <span className="glyphicon glyphicon-ok icon" aria-hidden="true"
+  onClick={this.onCliclDone}></span>
+  </div>
+ {this.props.item.value}
+</div>
+<div class="column">
+<button type="button" className="delete is-medium" onClick={this.onClickClose} >&times;</button>
+</div>
+</div>
+
+);
+
+  }
+
+}
+class TodoForm extends Component{
+  constructor(props){
+    super(props);
+    this.onSubmit= this.onSubmit.bind(this);
+
+  }
+  onSubmit(event)
+  {
+    event.preventDefault();
+    let newItemValue = this.refs.itemName.value;
+    if(newItemValue)
+    {
+      this.props.addItem({newItemValue});
+      this.refs.form.reset();
+    }
+
+  }
+
+  render()
+  {
     return(
-     <div>
-         <h2>My TodoList Components works !!!</h2>
-     </div>
+      <form ref="form" onSubmit={this.onSubmit} className="form-inline">
+       <input type="text" ref="itemName" className="form-control" placeholder="add new task..." />
+       <button type="submit" className="button is-default">ADD</button>
+      </form>
     );
   }
 }
-// export default Home;
+class TodoHeader extends Component {
+  render(){
+    return <h1>What do you want to do?</h1>
+  }
+}
+
+class TodoApp extends Component{
+constructor(props)
+{
+super(props);
+this.addItem=this.addItem.bind(this);
+this.removeitem=this.removeitem.bind(this);
+this.markDone=this.removeitem.bind(this);
+this.state={todoitems:todoitems};
+}
+
+addItem(todoitem){
+  todoitems.unshift({
+index: todoitems.length+1,
+value: todoitem.newItemValue,
+done: false
+  });
+  this.setState({todoitems: todoitems});
+
+
+}
+removeitem(itemIndex){
+ todoitems.splice(itemIndex,1);
+ this.setState({todoitems:todoitems});
+
+}
+markDone(itemIndex){
+let task= todoitems[itemIndex];
+todoitems.splice(itemIndex,1);
+task.done=!task.done;
+task.done ? todoitems.push(task):todoitems.unshift(task);
+this.setState({todoitems:todoitems});
+}
+
+render(){
+  return(
+      <div id="main">
+      <TodoHeader></TodoHeader>
+      <TodoList items={this.props.initItems} removeitem={this.removeitem} markDone={this.markDone}></TodoList>
+      <TodoForm addItem={this.addItem}></TodoForm>
+
+      </div>
+  )
+}
+
+
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TodoApp initItems={todoitems}></TodoApp>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,13 +1,78 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bulma/css/bulma.css';
 
-export default class Weather extends Component{
+
+
+const PLACES =[
+  {name:"Rivne",zip:"33000"},
+  {name:"Washington",zip:"20001"},
+  {name:"Los Angeles",zip:"90023"},
+  {name:"Seattle",zip:"98101"}
+];
+
+class WeatherService extends Component {
+constructor(){
+  super();
+  this.state={weatherData :null};
+}
+  componentDidMount(){
+     const zip =this.props.zip;
+     const apiUrl ="http://api.openweathermap.org/data/2.5/weather?q=" +
+                    zip +"&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+  
+   fetch(apiUrl)
+   .then( res => res.json())
+   .then( json => {this.setState({weatherData:json})})
+  }
 render(){
-    return(
-     <div>
-         <h2>My Weather Components works !!!</h2>
-     </div>
+  const weatherData=this.state.weatherData;
+  if(!weatherData)
+  return <div>Loading...</div>
+  const weather=weatherData.weather[0];
+  const iconURL ="http://api.openweathermap.org/img/w/" + weather.icon + ".png";
+  return(
+  // <div>{JSON.stringify(weatherData)}</div>
+  <div>
+    <h1>
+      {weather.main} in {weatherData.name}
+      <img src={iconURL} alt={weatherData.description}/> 
+    </h1>
+    <p>Current temp: {weatherData.main.temp} °C</p>
+    <p>Min temp: {weatherData.main.temp_min} °C</p>
+    <p>Max temp: {weatherData.main.temp_max} °C</p>
+    <p>Wind speed: {weatherData.wind.speed} m/sec</p>
+    <p>Pressure: {weatherData.main.pressure} pasc</p>
+
+
+  </div>
+  );
+}
+}
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      activePlace:0
+    };
+  }
+  render() {
+    const activePlace = this.state.activePlace;
+    return (
+      <div className="App">
+        <WeatherService zip={PLACES[activePlace].zip} key={activePlace} />
+        { PLACES.map((place,index)=>(
+          <button className={"button is-hovered"} key={index}
+           onClick={() =>{this.setState({activePlace:index})}}>
+             {/* /console.log(`Clicked ${index} button`))}>*/}
+           {place.name} 
+          </button>
+        ))}
+      </div>
     );
   }
 }
-// export default Home;
+
+export default App;
